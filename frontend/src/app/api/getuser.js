2bc521f513cf0842f6083f50e839b7353363e437
mam-ouni <salah.aissaoui@univ-constantine2.dz>
2024-06-06 23:26:08 +0200

    next back branched

--- frontend/src/app/api/getuser.js
+++
@@ -1,8 +0,0 @@
-import { connection } from "./db";
-export default async function handler(req, res) {
-    try {
-        const con = connection()
-    } catch (error) {
-        console.error("Error fetching users:", error);
-    }
-}
