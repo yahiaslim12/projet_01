7e022912276c1cbcde7376fb1c08258238add8f2
yahia <yahia.slimani@univ-constantine2.dz>
2024-06-06 16:39:26 +0100

    add backend to vercel

--- backend/server.py
+++ backend/server.py
@@ -7,29 +7,35 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://default:oLa5hePtz0RN@ep-proud-hill-a2s52c8d.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require"
 
 DATABASE_URL = app.config['SQLALCHEMY_DATABASE_URI']
-CORS(app,supports_credentials=True)
+CORS(app, supports_credentials=True)
+
 # Connect to PostgreSQL
 def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL)
-    if conn : print("connection successfuly")
+    if conn:
+        print("connection successfuly")
     return conn
+
+@app.route('/api/test')
+def test():
+    return jsonify({"message": "hello"})
 
 @app.route('/api/createAccount', methods=['POST'])
 def create_account():
     try:
         con = get_db_connection()
         data = request.get_json()
-        
+
         if not data:
             return jsonify({"error": "No input data provided"}), 400
-        
+
         email = data.get('email')
         password = data.get('password')
         firstname = data.get('firstname')
         lastname = data.get('lastname')
         age = data.get('age')
         weight = data.get('weight')
-        
+
         if not email or not password or not firstname or not lastname or not age or not weight:
             return jsonify({"error": "Missing required fields"}), 400
 
@@ -38,19 +44,14 @@ def create_account():
             "INSERT INTO users (firstname, lastname, age, weight, email, password) VALUES (%s, %s, %s, %s, %s, %s)",
             (firstname, lastname, age, weight, email, password)
         )
-        con.commit()  
-        cursor.close()  
-        con.close()  
+        con.commit()
+        cursor.close()
+        con.close()
 
         return jsonify({"message": "User is added"})
     except Exception as e:
         app.logger.error(f"Database error occurred: {e}")
         return jsonify({"error": "Database query failed"}), 500
-        
-    
-    
-
-
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True, port=8000)
