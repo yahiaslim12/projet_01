8fba66321f4dadaab1496e297e03d859aeaf6cc2
yahia <yahia.slimani@univ-constantine2.dz>
2024-06-07 12:41:09 +0100

    hak a aissa

---
+++ frontend/src/app/api/createAccount.js
@@ -0,0 +1,7 @@
+'use server'
+
+export default function createAccount() {
+   async function create(data){
+
+   }
+}
--- frontend/src/app/api/getuser/route.js
+++
@@ -1,8 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
- 
-export async function GET(request) {
-
-  const pets = await sql`SELECT * FROM users;`;
-  return NextResponse.json({ pets }, { status: 200 });
-}
